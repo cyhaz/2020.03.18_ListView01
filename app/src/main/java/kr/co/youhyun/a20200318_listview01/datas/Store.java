5e9e9f7f4e4aec8f45617fb40a544df1629db859
cyhaz <cyhaz@naver.com>
2020-03-18 11:43:21 +0900

    가게 정보를 다루는 클래스 추가

--- app/src/main/java/kr/co/youhyun/a20200318_listview01/datas/Store.java
+++ app/src/main/java/kr/co/youhyun/a20200318_listview01/datas/Store.java
@@ -1,4 +1,31 @@
 package kr.co.youhyun.a20200318_listview01.datas;
 
-public class Store {
+import java.io.Serializable;
+
+public class Store implements Serializable {
+
+    private String name;
+    private int rating;  //별을 몇개 찍어줘야 하는지 (평점)
+
+//    생성자와 게터세터 생성 (Alt + Insert)
+    public Store(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
